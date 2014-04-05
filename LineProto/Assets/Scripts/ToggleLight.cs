ba9bd0557ea67e3a81a3cd9fd8e9bbc680636f30
Justin Demetroff <demetroffj@gmail.com>
2014-04-05 12:13:32 -0400

    Initial Project Commit

---
+++ LineProto/Assets/Scripts/ToggleLight.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleLight : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider collider) 
+	{
+		Debug.Log ("On Trigger Enter");
+		if (collider.tag == "TheLine")
+		{
+			string currLight = gameObject.name; 
+			Debug.Log(gameObject.name);
+			GameObject lSource = GameObject.Find(currLight+"/Point light");
+			lSource.light.enabled = true;
+			//StartCoroutine(MyCoroutine(lSource));
+			Debug.Log(lSource.light.intensity);
+			//find child point light and enable light
+		}
+	}
+	/*IEnumerator MyCoroutine(GameObject lSource)
+	{
+		int lIntes = lSource.light.intensity
+		for(int i = lIntes; i >= 0; i--)
+		{
+				lSource.light.intensity -= 1;
+				yield return new WaitForSeconds (0.5f);
+		}
+	}*/
+}
