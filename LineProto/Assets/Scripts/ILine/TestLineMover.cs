0da1675a46f872239f67573bf57353cc6fe53564
Eagan Rackley <eagix@sbcglobal.net>
2014-06-16 10:57:15 -0400

    Added gizmos to repo so that we can be sure shader line matches up with wordlspace coordinates. I'm very worried about this...

--- LineProto/Assets/Scripts/ILine/TestLineMover.cs
+++ LineProto/Assets/Scripts/ILine/TestLineMover.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TestLineMover : MonoBehaviour {
 
-	Vector4 ShaderPoint = new Vector4(1.0f, -0.5f, 0.0f, 1.0f);
+	Vector4 ShaderPoint = new Vector4(1.0f, 0.0f, 0.0f, 1.0f);
 	public float MoveVelocity = 0.02f;
 	public float HMagnitude = 1.0f;
 	public float VMagnitude = 1.0f;
@@ -13,7 +13,11 @@ public class TestLineMover : MonoBehaviour {
 	private float myCount = 0.0f;
 	private float myMoveCount = 0.0f;
 	private float mySineOffset = 0.0f;
+	private float myMagnitudeCounter = 0.0f;
 	public Vector3 rot = new Vector3(0.0f, 1.0f, 0.0f);
+	private float hMag;
+	private float vMag;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -21,9 +25,10 @@ public class TestLineMover : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		myCount+=0.01f;
 		myMoveCount += MoveVelocity;
-		ShaderPoint.y = Mathf.Sin (myCount) * 1.0f;
+
 		if (ChangePosition) 
 		{
 			Vector3 pos = this.transform.position;
@@ -32,12 +37,29 @@ public class TestLineMover : MonoBehaviour {
 		}
 		if (ChangeShader) 
 		{
+			ShaderPoint.y = Mathf.Sin (myCount) * 1.0f;
+			myMagnitudeCounter += Time.deltaTime;
+			if(myMagnitudeCounter > 1.0f) myMagnitudeCounter = 0.0f;
+			hMag = HMagnitude; //Mathf.Lerp(0.0f, HMagnitude, myMagnitudeCounter);
+			vMag = Mathf.Lerp(0.0f, VMagnitude, Mathf.Sin (myCount/2));
 			renderer.sharedMaterial.SetVector("_LinePoint", ShaderPoint);
 			renderer.sharedMaterial.SetFloat("_SineOffset", mySineOffset);
-			renderer.sharedMaterial.SetFloat("_SineHMagnitude", HMagnitude);
-			renderer.sharedMaterial.SetFloat("_SineVMagnitude", VMagnitude);
+			renderer.sharedMaterial.SetFloat("_SineHMagnitude", hMag);
+			renderer.sharedMaterial.SetFloat("_SineVMagnitude", vMag);
 			mySineOffset += SineOffsetVel;
 		}
 		//this.transform.Rotate (rot);
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+
+		for (float x = -24.0f; x < 24.0f; x += 1.0f) 
+		{
+			Vector3 drawAt = new Vector3( x, transform.position.y, 0.0f );
+			drawAt.y = Mathf.Sin(mySineOffset + drawAt.x / (hMag * 23f)) * (vMag * 14f) + (ShaderPoint.y * 12f);
+			Gizmos.DrawCube (drawAt, new Vector3 (0.1f, 0.1f, 0.1f));
+		}
 	}
 }
